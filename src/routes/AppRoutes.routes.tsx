c40353584380a23f551a63c592b600439641ec61
Gustavotc <gustavotch@hotmail.com>
2025-02-17 21:51:41 -0300

    refact: remove react-router because it was not necessary

--- src/routes/AppRoutes.routes.tsx
+++ src/routes/AppRoutes.routes.tsx
@@ -1,29 +1,14 @@
+import React from 'react';
 import App from '@/App';
 import { useApp } from '@/context/AppContext';
 import Login from '@/pages/login/Login';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router';
 
 const AppRoutes: React.FC = () => {
   const { user } = useApp();
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        {user ? (
-          <Route
-            index
-            element={<App />}
-          />
-        ) : (
-          <Route
-            index
-            element={<Login />}
-          />
-        )}
-      </Routes>
-    </BrowserRouter>
-  );
+  if (user) return <App />;
+
+  return <Login />;
 };
 
 export default AppRoutes;
